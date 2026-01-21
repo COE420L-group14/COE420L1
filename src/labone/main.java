564d9fccced7b2799102895ec720fa967249e6b9
talaalhasan10 <g00102390@aus.edu>
2026-01-21 22:12:33 +0400

    Feature B added

--- src/labone/main.java
+++ src/labone/main.java
@@ -2,6 +2,8 @@ package labone;
 
 public class main {
     public static void main(String[] args) {
+	int x=70;
         System.out.println("hello world!!!");
+	System.out.println("how are you..? the weather is " + x + " cloudy today...");
     }
 }
