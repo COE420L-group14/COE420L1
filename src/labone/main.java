3f40b3aac14115050e00f73351d6d081d9ec7b11
FNikolova <g00097943@aus.edu>
2026-01-21 22:14:56 +0400

    Update weather output format in main.java

--- src/labone/main.java
+++ src/labone/main.java
@@ -4,6 +4,6 @@ public class main {
     public static void main(String[] args) {
 	int x=70;
         System.out.println("hello world!!!");
-	System.out.println("how are you..? the weather is " + x + " cloudy today...");
+	System.out.println("how are you..? the weather is " + x + " % cloudy today...");
     }
 }
